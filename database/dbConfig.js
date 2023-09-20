00efce6876a38c7c989fd0ff989431f6a02800ad
xiaoqingming18 <xiaoqingming18@163.com>
2023-09-20 14:28:53 +0800

    1.0.2

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 module.exports = {
     // 数据库配置
     config: {
-        host: 'localhost',
+        host: '127.0.0.1',
         port: 3306,
-        user: 'root',
-        password: '20020318ZXF',
+        user: 'zhifou',
+        password: 't6SHifKxwPTRirdk',
         database: 'zhifou'
     },
     sqlConnect: function(sql,sqlArr,callback) {
